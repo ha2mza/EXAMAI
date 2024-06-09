604d238bea3ec2984915f7184846441a66b97d4a
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2024-06-09 21:48:18 +0100

    script.sql

--- mysql-init/script.sql
+++ mysql-init/script.sql
@@ -5,124 +5,154 @@ FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS examai;
 USE examai;
 
-CREATE TABLE `classes` (
-                           `id` int NOT NULL AUTO_INCREMENT,
-                           `name` varchar(255) DEFAULT NULL,
-                           `year` int DEFAULT NULL,
-                           `action_user` int DEFAULT NULL,
-                           `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-                           `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                           `deleted_at` timestamp NULL DEFAULT NULL,
-                           PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `class_condidats` (
-                                   `id` int NOT NULL AUTO_INCREMENT,
-                                   `condidat_id` int DEFAULT NULL,
-                                   `class_id` int DEFAULT NULL,
-                                   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `condidats` (
-                             `id` int NOT NULL AUTO_INCREMENT,
-                             `first_name` varchar(255) DEFAULT NULL,
-                             `last_name` varchar(255) DEFAULT NULL,
-                             `email` varchar(255) DEFAULT NULL,
-                             `action_user` int DEFAULT NULL,
-                             `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-                             `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                             `deleted_at` timestamp NULL DEFAULT NULL,
-                             PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+SET time_zone = "+00:00";
 
-CREATE TABLE `condidat_answers` (
-                                    `id` int NOT NULL AUTO_INCREMENT,
-                                    `exam_id` int DEFAULT NULL,
-                                    `version_id` int DEFAULT NULL,
-                                    `question_id` int DEFAULT NULL,
-                                    `reponse` longtext,
-                                    `correct` tinyint(1) DEFAULT '0',
-                                    `condidat_id` int DEFAULT NULL,
-                                    PRIMARY KEY (`id`)
+CREATE TABLE `candidat` (
+                            `ID_CANDIDAT` int(11) NOT NULL AUTO_INCREMENT,
+                            `NOM` varchar(75) DEFAULT NULL,
+                            `PRENOM` varchar(75) DEFAULT NULL,
+                            `MOT_DE_PASSE` varchar(40) DEFAULT NULL,
+                            `EMAIL` varchar(30) DEFAULT NULL,
+                            PRIMARY KEY (`ID_CANDIDAT`)
 );
 
-CREATE TABLE `exams` (
-                         `id` int NOT NULL AUTO_INCREMENT,
-                         `type` varchar(120) DEFAULT NULL,
-                         `nature` varchar(255) DEFAULT NULL,
-                         `module_id` int DEFAULT NULL,
-                         `duree` int DEFAULT '60',
-                         `passed_at` datetime DEFAULT NULL,
-                         `action_user` int DEFAULT NULL,
-                         `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-                         `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                         `deleted_at` timestamp NULL DEFAULT NULL,
-                         `class_id` int DEFAULT NULL,
-                         `number_version` int DEFAULT '1',
-                         `change_qcm_order` tinyint(1) DEFAULT NULL,
-                         `change_condidats_order` tinyint(1) DEFAULT NULL,
-                         `strict_date` tinyint(1) DEFAULT '1',
-                         `status` varchar(120) DEFAULT NULL,
-                         PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-CREATE TABLE `exam_versions` (
-                                 `id` int NOT NULL AUTO_INCREMENT,
-                                 `exam_id` int DEFAULT NULL,
-                                 `question` longtext,
-                                 PRIMARY KEY (`id`)
+CREATE TABLE `candidat_classe` (
+                                   `id` int(11) NOT NULL AUTO_INCREMENT,
+                                   `ID_CANDIDAT` int(11) NOT NULL,
+                                   `ID_CLASSE` int(11) NOT NULL,
+                                   PRIMARY KEY (`id`),
+                                   FOREIGN KEY (`ID_CANDIDAT`) REFERENCES `candidat` (`ID_CANDIDAT`),
+                                   FOREIGN KEY (`ID_CLASSE`) REFERENCES `classe` (`ID_CLASSE`)
 );
 
-CREATE TABLE `exam_version_condidats` (
-                                          `id` int NOT NULL AUTO_INCREMENT,
-                                          `version_id` int DEFAULT NULL,
-                                          `condidat_id` int DEFAULT NULL,
-                                          `code` varchar(250) DEFAULT NULL,
-                                          `sent_at` timestamp NULL DEFAULT NULL,
-                                          `note` double DEFAULT NULL,
-                                          `started_at` timestamp NULL DEFAULT NULL,
-                                          `ended_at` timestamp NULL DEFAULT NULL,
-                                          PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-CREATE TABLE `modules` (
-                           `id` int NOT NULL AUTO_INCREMENT,
-                           `name` varchar(250) DEFAULT NULL,
-                           `action_user` int DEFAULT NULL,
-                           `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                           `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                           `deleted_at` timestamp NULL DEFAULT NULL,
-                           PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-CREATE TABLE `module_qcms` (
-                               `id` int NOT NULL AUTO_INCREMENT,
-                               `module_id` int DEFAULT NULL,
-                               `title` text,
-                               `choice` longtext,
-                               `action_user` int DEFAULT NULL,
-                               `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-                               `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                               `deleted_at` timestamp NULL DEFAULT NULL,
-                               `qcm_type_id` int DEFAULT NULL,
-                               PRIMARY KEY (`id`)
+CREATE TABLE `candidat_passage` (
+                                    `id` int(11) NOT NULL AUTO_INCREMENT,
+                                    `ID_CANDIDAT` int(11) NOT NULL,
+                                    `ID_EXAMEN` int(11) NOT NULL,
+                                    `CODE` varchar(50) DEFAULT NULL,
+                                    `ENVOYE_A` datetime DEFAULT NULL,
+                                    `NOTE` float DEFAULT NULL,
+                                    `COMMENCE_A` datetime DEFAULT NULL,
+                                    `TERMINE_A` datetime DEFAULT NULL,
+                                    `LIEN` varchar(128) DEFAULT NULL,
+                                    PRIMARY KEY (`id`),
+                                    FOREIGN KEY (`ID_CANDIDAT`) REFERENCES `candidat` (`ID_CANDIDAT`),
+                                    FOREIGN KEY (`ID_EXAMEN`) REFERENCES `examen` (`ID_EXAMEN`)
 );
 
-CREATE TABLE `qcm_type` (
-                            `id` int NOT NULL AUTO_INCREMENT,
-                            `name` varchar(250) DEFAULT NULL,
-                            `code` varchar(250) DEFAULT NULL,
-                            PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE `candidat_repondre` (
+                                     `id` int(11) NOT NULL AUTO_INCREMENT,
+                                     `ID_CANDIDAT` int(11) NOT NULL,
+                                     `ID_QUESTION` int(11) NOT NULL,
+                                     `ID_EXAMEN` int(11) NOT NULL,
+                                     `REPONDRE` text DEFAULT NULL,
+                                     PRIMARY KEY (`id`),
+                                     FOREIGN KEY (`ID_CANDIDAT`) REFERENCES `candidat` (`ID_CANDIDAT`),
+                                     FOREIGN KEY (`ID_QUESTION`) REFERENCES `question` (`ID_QUESTION`),
+                                     FOREIGN KEY (`ID_EXAMEN`) REFERENCES `examen` (`ID_EXAMEN`)
+);
 
-CREATE TABLE `teachers` (
-                            `id` int NOT NULL AUTO_INCREMENT,
-                            `first_name` varchar(255) DEFAULT NULL,
-                            `last_name` varchar(255) DEFAULT NULL,
-                            `email` varchar(255) DEFAULT NULL,
-                            `password` varchar(255) DEFAULT NULL,
-                            `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-                            `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                            `deleted_at` timestamp NULL DEFAULT NULL,
-                            PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE `classe` (
+                          `ID_CLASSE` int(11) NOT NULL AUTO_INCREMENT,
+                          `NOM_CLASSE` varchar(50) DEFAULT NULL,
+                          `ANNEE` int(11) DEFAULT NULL,
+                          `CODE` varchar(50) DEFAULT NULL,
+                          `ID_ENSEIGNANT` int(11) DEFAULT NULL,
+                          PRIMARY KEY (`ID_CLASSE`),
+                          FOREIGN KEY (`ID_ENSEIGNANT`) REFERENCES `enseignant` (`ID_ENSEIGNANT`)
+);
+
+CREATE TABLE `cour` (
+                        `ID_COUR` int(11) NOT NULL AUTO_INCREMENT,
+                        `ID_ENSEIGNANT` int(11) NOT NULL,
+                        `NOM_COUR` varchar(50) DEFAULT NULL,
+                        PRIMARY KEY (`ID_COUR`),
+                        FOREIGN KEY (`ID_ENSEIGNANT`) REFERENCES `enseignant` (`ID_ENSEIGNANT`)
+);
+
+CREATE TABLE `enseignant` (
+                              `ID_ENSEIGNANT` int(11) NOT NULL AUTO_INCREMENT,
+                              `NOM_ENSEIGNANT` varchar(75) DEFAULT NULL,
+                              `PRENOM_ENSEIGNANT` varchar(75) DEFAULT NULL,
+                              `MOT_DE_PASSE` varchar(40) DEFAULT NULL,
+                              `EMAIL` varchar(30) DEFAULT NULL,
+                              PRIMARY KEY (`ID_ENSEIGNANT`)
+);
+
+CREATE TABLE `examen` (
+                          `ID_EXAMEN` int(11) NOT NULL AUTO_INCREMENT,
+                          `ID_ENSEIGNANT` int(11) NOT NULL,
+                          `ID_CLASSE` int(11) NOT NULL,
+                          `ID_COUR` int(11) DEFAULT NULL,
+                          `TYPE` varchar(100) DEFAULT NULL,
+                          `NATURE` varchar(100) DEFAULT NULL,
+                          `DUREE` int(11) DEFAULT NULL,
+                          `NB_DE_VERSION` int(11) DEFAULT NULL,
+                          `STATUT` varchar(255) DEFAULT NULL,
+                          `DATE_STRICTE` tinyint(1) DEFAULT NULL,
+                          `QUESTIONS` text DEFAULT NULL,
+                          `COMMENCE_A` datetime DEFAULT NULL,
+                          `ORDRE_QUESTION` tinyint(1) DEFAULT NULL,
+                          `ORDRE_CHOIX` tinyint(1) DEFAULT NULL,
+                          `CREERA` timestamp NULL DEFAULT current_timestamp(),
+                          PRIMARY KEY (`ID_EXAMEN`),
+                          FOREIGN KEY (`ID_ENSEIGNANT`) REFERENCES `enseignant` (`ID_ENSEIGNANT`),
+                          FOREIGN KEY (`ID_CLASSE`) REFERENCES `classe` (`ID_CLASSE`),
+                          FOREIGN KEY (`ID_COUR`) REFERENCES `cour` (`ID_COUR`)
+);
+
+CREATE TABLE `question` (
+                            `ID_QUESTION` int(11) NOT NULL AUTO_INCREMENT,
+                            `ID_COUR` int(11) NOT NULL,
+                            `ID_QUESTION_TYPE` int(11) NOT NULL,
+                            `TITRE` text DEFAULT NULL,
+                            `CHOIX` text DEFAULT NULL,
+                            `POSITION` int(11) DEFAULT NULL,
+                            PRIMARY KEY (`ID_QUESTION`),
+                            FOREIGN KEY (`ID_COUR`) REFERENCES `cour` (`ID_COUR`),
+                            FOREIGN KEY (`ID_QUESTION_TYPE`) REFERENCES `question_type` (`ID_QUESTION_TYPE`)
+);
+
+CREATE TABLE `question_type` (
+                                 `ID_QUESTION_TYPE` int(11) NOT NULL AUTO_INCREMENT,
+                                 `NOM` varchar(75) DEFAULT NULL,
+                                 `CODE` varchar(50) DEFAULT NULL,
+                                 PRIMARY KEY (`ID_QUESTION_TYPE`)
+);
+
+CREATE VIEW `candidat_repd` AS
+SELECT `candidat_repondre`.`id` AS `id`,
+       `candidat_repondre`.`ID_CANDIDAT` AS `ID_CANDIDAT`,
+       `candidat_repondre`.`ID_QUESTION` AS `ID_QUESTION`,
+       `candidat_repondre`.`ID_EXAMEN` AS `ID_EXAMEN`,
+       `candidat_repondre`.`REPONDRE` AS `REPONDRE`,
+       `opt`.`reponse` AS `reponse`
+FROM (`candidat_repondre`
+         JOIN JSON_TABLE(`candidat_repondre`.`REPONDRE`, '$[*]' COLUMNS (`reponse` int(11) PATH '$' DEFAULT '' ON EMPTY DEFAULT 'N' ON ERROR)) `opt`);
+
+CREATE VIEW `options` AS
+SELECT `question`.`ID_QUESTION` AS `ID_QUESTION`,
+       `option`.`rowid` AS `rowid`,
+       `option`.`titre` AS `titre`,
+       `option`.`correct` AS `correct`
+FROM (`question`
+         JOIN JSON_TABLE(`question`.`CHOIX`, '$[*]' COLUMNS (`rowid` int(11) PATH '$.index', `titre` varchar(100) PATH '$.titre' DEFAULT '' ON EMPTY DEFAULT 'N' ON ERROR, `correct` tinyint(1) PATH '$.correct')) `option`);
+
+CREATE VIEW `reponse` AS
+SELECT `candidat_repondre`.`id` AS `id`,
+       `candidat_repondre`.`ID_CANDIDAT` AS `ID_CANDIDAT`,
+       `candidat_repondre`.`ID_QUESTION` AS `ID_QUESTION`,
+       `candidat_repondre`.`ID_EXAMEN` AS `ID_EXAMEN`,
+       `candidat_repondre`.`REPONDRE` AS `REPONDRE`,
+       `option_correct`.`rep` AS `rep`,
+       `option_correct`.`rep`= `candidat_repondre`.`REPONDRE` AS `correct`
+FROM (`candidat_repondre`
+         JOIN (SELECT `options`.`ID_QUESTION` AS `ID_QUESTION`,
+                      CONCAT('[', COALESCE(GROUP_CONCAT(CONCAT('"', `options`.`rowid` ,'"') ORDER BY `options`.`rowid` DESC SEPARATOR ','), ''), ']') AS `rep`
+               FROM `options`
+               WHERE `options`.`correct` = 1
+               GROUP BY `options`.`ID_QUESTION`
+               ORDER BY `options`.`rowid`) `option_correct`)
+WHERE `option_correct`.`ID_QUESTION` = `candidat_repondre`.`ID_QUESTION`;
