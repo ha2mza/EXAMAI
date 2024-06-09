ad82473aed18233ac5070e3cd16d445309298023
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2024-06-09 21:54:55 +0100

    script.sql

--- mysql-init/script.sql
+++ mysql-init/script.sql
@@ -6,8 +6,16 @@ CREATE DATABASE IF NOT EXISTS examai;
 USE examai;
 
 
-
 SET time_zone = "+00:00";
+
+CREATE TABLE `enseignant` (
+                              `ID_ENSEIGNANT` int(11) NOT NULL AUTO_INCREMENT,
+                              `NOM_ENSEIGNANT` varchar(75) DEFAULT NULL,
+                              `PRENOM_ENSEIGNANT` varchar(75) DEFAULT NULL,
+                              `MOT_DE_PASSE` varchar(40) DEFAULT NULL,
+                              `EMAIL` varchar(30) DEFAULT NULL,
+                              PRIMARY KEY (`ID_ENSEIGNANT`)
+);
 
 CREATE TABLE `candidat` (
                             `ID_CANDIDAT` int(11) NOT NULL AUTO_INCREMENT,
@@ -18,40 +26,11 @@ CREATE TABLE `candidat` (
                             PRIMARY KEY (`ID_CANDIDAT`)
 );
 
-CREATE TABLE `candidat_classe` (
-                                   `id` int(11) NOT NULL AUTO_INCREMENT,
-                                   `ID_CANDIDAT` int(11) NOT NULL,
-                                   `ID_CLASSE` int(11) NOT NULL,
-                                   PRIMARY KEY (`id`),
-                                   FOREIGN KEY (`ID_CANDIDAT`) REFERENCES `candidat` (`ID_CANDIDAT`),
-                                   FOREIGN KEY (`ID_CLASSE`) REFERENCES `classe` (`ID_CLASSE`)
-);
-
-CREATE TABLE `candidat_passage` (
-                                    `id` int(11) NOT NULL AUTO_INCREMENT,
-                                    `ID_CANDIDAT` int(11) NOT NULL,
-                                    `ID_EXAMEN` int(11) NOT NULL,
-                                    `CODE` varchar(50) DEFAULT NULL,
-                                    `ENVOYE_A` datetime DEFAULT NULL,
-                                    `NOTE` float DEFAULT NULL,
-                                    `COMMENCE_A` datetime DEFAULT NULL,
-                                    `TERMINE_A` datetime DEFAULT NULL,
-                                    `LIEN` varchar(128) DEFAULT NULL,
-                                    PRIMARY KEY (`id`),
-                                    FOREIGN KEY (`ID_CANDIDAT`) REFERENCES `candidat` (`ID_CANDIDAT`),
-                                    FOREIGN KEY (`ID_EXAMEN`) REFERENCES `examen` (`ID_EXAMEN`)
-);
-
-CREATE TABLE `candidat_repondre` (
-                                     `id` int(11) NOT NULL AUTO_INCREMENT,
-                                     `ID_CANDIDAT` int(11) NOT NULL,
-                                     `ID_QUESTION` int(11) NOT NULL,
-                                     `ID_EXAMEN` int(11) NOT NULL,
-                                     `REPONDRE` text DEFAULT NULL,
-                                     PRIMARY KEY (`id`),
-                                     FOREIGN KEY (`ID_CANDIDAT`) REFERENCES `candidat` (`ID_CANDIDAT`),
-                                     FOREIGN KEY (`ID_QUESTION`) REFERENCES `question` (`ID_QUESTION`),
-                                     FOREIGN KEY (`ID_EXAMEN`) REFERENCES `examen` (`ID_EXAMEN`)
+CREATE TABLE `question_type` (
+                                 `ID_QUESTION_TYPE` int(11) NOT NULL AUTO_INCREMENT,
+                                 `NOM` varchar(75) DEFAULT NULL,
+                                 `CODE` varchar(50) DEFAULT NULL,
+                                 PRIMARY KEY (`ID_QUESTION_TYPE`)
 );
 
 CREATE TABLE `classe` (
@@ -70,15 +49,6 @@ CREATE TABLE `cour` (
                         `NOM_COUR` varchar(50) DEFAULT NULL,
                         PRIMARY KEY (`ID_COUR`),
                         FOREIGN KEY (`ID_ENSEIGNANT`) REFERENCES `enseignant` (`ID_ENSEIGNANT`)
-);
-
-CREATE TABLE `enseignant` (
-                              `ID_ENSEIGNANT` int(11) NOT NULL AUTO_INCREMENT,
-                              `NOM_ENSEIGNANT` varchar(75) DEFAULT NULL,
-                              `PRENOM_ENSEIGNANT` varchar(75) DEFAULT NULL,
-                              `MOT_DE_PASSE` varchar(40) DEFAULT NULL,
-                              `EMAIL` varchar(30) DEFAULT NULL,
-                              PRIMARY KEY (`ID_ENSEIGNANT`)
 );
 
 CREATE TABLE `examen` (
@@ -115,11 +85,40 @@ CREATE TABLE `question` (
                             FOREIGN KEY (`ID_QUESTION_TYPE`) REFERENCES `question_type` (`ID_QUESTION_TYPE`)
 );
 
-CREATE TABLE `question_type` (
-                                 `ID_QUESTION_TYPE` int(11) NOT NULL AUTO_INCREMENT,
-                                 `NOM` varchar(75) DEFAULT NULL,
-                                 `CODE` varchar(50) DEFAULT NULL,
-                                 PRIMARY KEY (`ID_QUESTION_TYPE`)
+CREATE TABLE `candidat_classe` (
+                                   `id` int(11) NOT NULL AUTO_INCREMENT,
+                                   `ID_CANDIDAT` int(11) NOT NULL,
+                                   `ID_CLASSE` int(11) NOT NULL,
+                                   PRIMARY KEY (`id`),
+                                   FOREIGN KEY (`ID_CANDIDAT`) REFERENCES `candidat` (`ID_CANDIDAT`),
+                                   FOREIGN KEY (`ID_CLASSE`) REFERENCES `classe` (`ID_CLASSE`)
+);
+
+CREATE TABLE `candidat_passage` (
+                                    `id` int(11) NOT NULL AUTO_INCREMENT,
+                                    `ID_CANDIDAT` int(11) NOT NULL,
+                                    `ID_EXAMEN` int(11) NOT NULL,
+                                    `CODE` varchar(50) DEFAULT NULL,
+                                    `ENVOYE_A` datetime DEFAULT NULL,
+                                    `NOTE` float DEFAULT NULL,
+                                    `COMMENCE_A` datetime DEFAULT NULL,
+                                    `TERMINE_A` datetime DEFAULT NULL,
+                                    `LIEN` varchar(128) DEFAULT NULL,
+                                    PRIMARY KEY (`id`),
+                                    FOREIGN KEY (`ID_CANDIDAT`) REFERENCES `candidat` (`ID_CANDIDAT`),
+                                    FOREIGN KEY (`ID_EXAMEN`) REFERENCES `examen` (`ID_EXAMEN`)
+);
+
+CREATE TABLE `candidat_repondre` (
+                                     `id` int(11) NOT NULL AUTO_INCREMENT,
+                                     `ID_CANDIDAT` int(11) NOT NULL,
+                                     `ID_QUESTION` int(11) NOT NULL,
+                                     `ID_EXAMEN` int(11) NOT NULL,
+                                     `REPONDRE` text DEFAULT NULL,
+                                     PRIMARY KEY (`id`),
+                                     FOREIGN KEY (`ID_CANDIDAT`) REFERENCES `candidat` (`ID_CANDIDAT`),
+                                     FOREIGN KEY (`ID_QUESTION`) REFERENCES `question` (`ID_QUESTION`),
+                                     FOREIGN KEY (`ID_EXAMEN`) REFERENCES `examen` (`ID_EXAMEN`)
 );
 
 CREATE VIEW `candidat_repd` AS
